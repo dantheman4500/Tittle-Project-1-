aef1fd440bee8960ad225b2bc3acb6ca60c03b8b
Simon Soliven <simonsolivennn@gmail.com>
2022-08-22 19:58:11 -0700

    commit

---
+++ assets/js/display.js
@@ -0,0 +1,22 @@
+//js for display
+
+const Songs = [
+"Heard 'Em Say - Kanye West", 
+"Les - Childish Gambino", 
+"Change - J. Cole", 
+"20 Something - SZA", 
+"Provider - Frank Ocean"];
+
+function generateListItems(arg) {
+        let items = "";
+        for(let i = 0; i < arg.length; i++) {
+            items += `<li>${arg[i]}</li>`;
+        }
+        return items;
+}
+
+document.querySelector("main").innerHTML = `
+<ol>
+${generateListItems(Songs)}
+</ol>
+`;
